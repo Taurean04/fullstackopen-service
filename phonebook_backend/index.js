c74d747d10a5690640d94a4a92d7beca36e55536
taurean04 <ayobamitomi@gmail.com>
2021-12-30 15:14:35 +0100

    add frontend build

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'))
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
