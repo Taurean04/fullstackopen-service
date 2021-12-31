e18ee3703b7df553b547126671fccb10506cc3c3
taurean04 <ayobamitomi@gmail.com>
2021-12-31 17:18:09 +0100

    uibuild

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -5,103 +5,104 @@ const app = express();
 require('dotenv').config();
 const Person = require('./models/person');
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
+	return JSON.stringify(req.body);
+});
 
 app.use(morgan(':method :url :body'));
 
 app.get('/', (req, res) => res.send('<h1>PhoneBook Service</h1>'));
 
 app.get('/api/persons', (req, res, next) => {
-  Person.find({})
-  .then(persons => {
-    res.json(persons);
-  })
-  .catch(e => next(e));
+	Person.find({})
+		.then(persons => {
+			res.json(persons);
+		})
+		.catch(e => next(e));
 });
 
 app.get('/info', (req, res, next) => {
-  Person.find({})
-  .then(persons => {
-    let info = {
-      message: `Phonebook has info for ${persons.length} people`,
-      date: new Date().toString()
-    }
-    res.json(info);
-  })
-  .catch(e => next(e));
+	Person.find({})
+		.then(persons => {
+			let info = {
+				message: `Phonebook has info for ${persons.length} people`,
+				date: new Date().toString()
+			};
+			res.json(info);
+		})
+		.catch(e => next(e));
 });
 
-app.get('/api/persons/:id', (req, res) => {  
-  Person.findById(req.params.id)
-  .then(person => {
-    if(!person){
-      res.status(404).end();
-    }
-    res.json(person);
-  }).catch(e => next(e));
+app.get('/api/persons/:id', (req, res, next) => {
+	Person.findById(req.params.id)
+		.then(person => {
+			if(!person){
+				res.status(404).end();
+			}
+			res.json(person);
+		}).catch(e => next(e));
 });
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-  .then(result => {
-    console.log(result);
-    res.status(204).end();
-  })
-  .catch(e => next(e));
+	Person.findByIdAndRemove(req.params.id)
+		.then(result => {
+			console.log(result);
+			res.status(204).end();
+		})
+		.catch(e => next(e));
 });
 
 app.post('/api/persons', (req, res, next) => {
-  let name = req.body.name;
-  let number = req.body.number;
-  
-  if(!name || !number){
-    return res.status(400).json({error: 'Person name or number is missing'});
-  }
+	let name = req.body.name;
+	let number = req.body.number;
 
-  const person = new Person({
-    name,
-    number
-  });
+	if(!name || !number){
+		return res.status(400).json({ error: 'Person name or number is missing' });
+	}
 
-  person.save()
-  .then(saved => {
-    console.log(`Added ${person.name} number ${person.number} to  phonebook`);
-    res.json(saved);
-  })
-  .catch(e => next(e));
+	const person = new Person({
+		name,
+		number
+	});
+
+	person.save()
+		.then(saved => {
+			console.log(`Added ${person.name} number ${person.number} to  phonebook`);
+			res.json(saved);
+		})
+		.catch(e => next(e));
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body;
+	const body = req.body;
 
-  Person.findByIdAndUpdate(req.params.id, {number: body.number}, { new: true })
-  .then(updated => {
-    res.json(updated);
-  })
-  .catch(e => next(e));
+	Person.findByIdAndUpdate(req.params.id, { number: body.number }, { new: true })
+		.then(updated => {
+			res.json(updated);
+		})
+		.catch(e => next(e));
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-}
+	response.status(404).send({ error: 'unknown endpoint' });
+};
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+	console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted ID' });
-  }else if (error.name === 'ValidationError') {    
-    return res.status(400).send({ error: error.message });
-  }
-  next(error);
-}
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted ID' });
+	}else if (error.name === 'ValidationError') {
+		return res.status(400).send({ error: error.message });
+	}
+	next(error);
+};
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
--- phonebook_backend/mongo.js
+++ phonebook_backend/mongo.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose');
 
+// eslint-disable-next-line no-undef
 if(process.argv.length < 3){
-    console.log('Please provide the password as an argument: node mongo.js <password>');
-    process.exit(1);
+	console.log('Please provide the password as an argument: node mongo.js <password>');
+	// eslint-disable-next-line no-undef
+	process.exit(1);
 }
 
+// eslint-disable-next-line no-undef
 const password = process.argv[2];
 let name, number;
 
@@ -13,28 +17,28 @@ const url = `mongodb+srv://root:${password}@cluster0.1ud5r.mongodb.net/phonebook
 mongoose.connect(url).catch(e => console.log(e));
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+	name: String,
+	number: String
 });
 
 const Person = mongoose.model('Person', personSchema);
 
 if(process.argv.length > 3) {
-  name = process.argv[3];
-  number = process.argv[4];
+	name = process.argv[3];
+	number = process.argv[4];
 
-  const person = new Person({ name, number });
-  
-  person.save().then(res => {
-      console.log(`Added ${name} number ${number} to  phonebook`);
-      mongoose.connection.close();
-  });
+	const person = new Person({ name, number });
+
+	person.save().then(() => {
+		console.log(`Added ${name} number ${number} to  phonebook`);
+		mongoose.connection.close();
+	});
 }else {
-  Person.find({}).then(res => {
-    console.log(`phonebook:`);
-    res.forEach(person => {
-      console.log(person);
-    });
-    mongoose.connection.close();
-  });
+	Person.find({}).then(res => {
+		console.log('phonebook:');
+		res.forEach(person => {
+			console.log(person);
+		});
+		mongoose.connection.close();
+	});
 }
