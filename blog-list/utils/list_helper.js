5887800b41a499c53c1f54fe3b314b298b966af4
taurean04 <ayobamitomi@gmail.com>
2022-01-03 15:10:16 +0100

    helper functions and unit tests

---
+++ blog-list/utils/list_helper.js
@@ -0,0 +1,33 @@
+const _ = require('lodash');
+
+const dummy = blogs => {
+    return 1;
+}
+
+const totalLikes = blogs => {
+    let total = 0;
+    blogs.map(blog => total += blog.likes);
+    return total;
+}
+
+const favoriteBlog = blogs => {
+    let maxObj = blogs.reduce((max, obj) => (max.likes > obj.likes) ? max : obj);
+    delete maxObj._id;
+    delete maxObj.url;
+    delete maxObj.__v;
+    return maxObj;
+}
+
+const mostBlogs = blogs => {
+    let most = _.countBy(blogs, 'author');
+    let max = Object.keys(most).reduce((a, b) => (most[a] > most[b]) ? a : b);
+    return { author: max, blogs: most[max] };
+}
+
+const mostLikes = blogs => {
+    let favorite_blog = favoriteBlog(blogs);
+    const { author, likes } = favorite_blog;
+    return { author, likes }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
